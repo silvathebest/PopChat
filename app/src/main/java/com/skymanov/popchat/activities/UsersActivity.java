9a0edb9d46748ee70a1e597434406b077db6b197
alexei <silverchat228@gmail.com>
2021-12-12 18:58:17 +0400

    users activity is ready

---
+++ app/src/main/java/com/skymanov/popchat/activities/UsersActivity.java
@@ -0,0 +1,85 @@
+package com.skymanov.popchat.activities;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.skymanov.popchat.adapters.UsersAdapter;
+import com.skymanov.popchat.databinding.ActivityUsersBinding;
+import com.skymanov.popchat.models.User;
+import com.skymanov.popchat.utilities.Constants;
+import com.skymanov.popchat.utilities.PreferenceManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsersActivity extends AppCompatActivity {
+    private ActivityUsersBinding binding;
+    private PreferenceManager preferenceManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityUsersBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        setListeners();
+        getUsers();
+    }
+
+    private void setListeners() {
+        binding.imageBack.setOnClickListener(v -> onBackPressed());
+    }
+
+    private void getUsers() {
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS).get().addOnCompleteListener(task -> {
+            loading(false);
+            String currentUserId = preferenceManager.getString(Constants.KEY_USER_ID);
+
+            if (task.isSuccessful() && task.getResult() != null) {
+                List<User> users = new ArrayList<>();
+
+                for (QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()) {
+                    if (currentUserId.equals(queryDocumentSnapshot.getId())) continue;
+
+                    User user = new User();
+                    user.name = queryDocumentSnapshot.getString(Constants.KEY_NAME);
+                    user.email = queryDocumentSnapshot.getString(Constants.KEY_EMAIL);
+                    user.image = queryDocumentSnapshot.getString(Constants.KEY_IMAGE);
+                    user.token = queryDocumentSnapshot.getString(Constants.KEY_FCM_TOKEN);
+
+                    users.add(user);
+                }
+
+                if (users.size() > 0) {
+                    UsersAdapter usersAdapter = new UsersAdapter(users);
+                    binding.userRecyclerView.setAdapter(usersAdapter);
+                    binding.userRecyclerView.setVisibility(View.VISIBLE);
+                } else {
+                    showErrorMessage();
+                }
+
+            } else {
+                showErrorMessage();
+            }
+        });
+    }
+
+    private void showErrorMessage() {
+        binding.textErrorMessage.setText(String.format("%s", "No user available"));
+        binding.textErrorMessage.setVisibility(View.VISIBLE);
+    }
+
+    private void loading(Boolean isLoading) {
+        if (isLoading) {
+            binding.progressbar.setVisibility(View.VISIBLE);
+            return;
+        }
+        binding.progressbar.setVisibility(View.INVISIBLE);
+    }
+}
---
+++ app/src/main/java/com/skymanov/popchat/models/User.java
@@ -0,0 +1,7 @@
+package com.skymanov.popchat.models;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    public String name, image, email, token;
+}
